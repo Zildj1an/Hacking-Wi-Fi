17fa5f90802b9e394846308c20170adfb8f9c63d
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2017-10-16 23:00:29 +0200

    Create fancy.sh
    This is a fancy version of the script

---
+++ fancy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+interface=`iwconfig 2>&1 | awk '{print $1}' | grep wlp`
+
+cowsay -f eyes 'HACKING-WIFI' | toilet --gay -f term
+sudo espeak "Are you ready for hacking wi-fi?"
+
+ifconfig
+
+sudo ifconfig $interface down 
+sudo macchanger -r $interface 
+sudo ifconfig $interface up 
+
+ifconfig
+
+
+cowsay -f bunny MAC cambiada 
+
+
+sudo airmon-ng start $interface 
+sudo airmon-ng check kill 
+sudo espeak "Now... please, select your victim"
+
+interface_mon=`iwconfig 2>&1 | awk '{print $1}' | grep mon`
+
+
+sudo airodump-ng $interface_mon
